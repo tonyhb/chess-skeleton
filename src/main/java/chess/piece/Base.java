788a6d65bc49d2035107fb44f1c7f9e59d889b79
Tony Holdstock-Brown <tonyhb@gmail.com>
2014-01-17 22:38:12 -0500

    Update Base piece class with a MoveBuilder helper
    This helper lets us create a new MoveBuilder for the current piece, with the
    piece and origin properties already set. A 3 liner becomes a 1 liner :)
    
    We've also separated showMoves into two methods - findPossibleMoves(), which
    lists all possible moves as an array of Positions, and showMoves() which
    returns an array of strings as required by the "list" command.
    
    Also fix indentations caused by IntelliJ (what the ****? this editor is
    crazy!).

--- src/main/java/chess/piece/Base.java
+++ src/main/java/chess/piece/Base.java
@@ -1,5 +1,6 @@
 package chess.piece;
 import chess.*;
+import chess.MoveBuilder.MoveBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,52 +8,64 @@ import java.util.List;
 /**
  * Created by Tony Holdstock-Brown on 10/01/2014.
  */
-    abstract public class Base {
-        protected Type type;
+abstract public class Base {
+    protected Type type;
 
-        protected Player player;
-        protected boolean moved;
+    protected Player player;
+    protected boolean moved;
 
-        protected Base() {
-            this.moved = false;
+    protected Base() {
+        this.moved = false;
+    }
+
+    public Base setPlayer(Player player) throws RuntimeException {
+        if (this.player == null) {
+            this.player = player;
+        } else {
+            throw new RuntimeException("Player has already been set");
         }
 
-        public Base setPlayer(Player player) throws RuntimeException {
-            if (this.player == null) {
-                this.player = player;
-            } else {
-                throw new RuntimeException("Player has already been set");
-            }
+        return this;
+    }
 
-            return this;
+    public Player getPlayer() {
+                            return this.player;
+                                                               }
+
+    public boolean hasMoved() {
+        return this.moved;
+    }
+
+    public Type getType() {
+                        return type;
+                                                }
+
+    public String toString() {
+        // If there's no abbreviation set, use the first letter of the piece type
+        String abbr = this.type.getLetter();
+
+        if (this.player == Player.White) {
+            abbr = abbr.toLowerCase();
+        } else {
+            abbr = abbr.toUpperCase();
         }
 
-        public Player getPlayer() {
-            return this.player;
-        }
+        return abbr;
+    }
 
-        public boolean hasMoved() {
-            return this.moved;
-        }
+    public abstract List<String> listMoves(String position);
 
-        public Type getType() {
-            return type;
-        }
+    public abstract List<Position> findPossibleMoves(String position);
 
-        public String toString() {
-            // If there's no abbreviation set, use the first letter of the piece type
-            String abbr = this.type.getLetter();
+    /**
+     * Returns a new MoveBuilder with the origin and piece auto-filled. Convenience function for listMoves().
+     *
+     * @param pos  String of the current piece's location
+     * @return     A MoveBuilder instance with origin and piece set.
+     */
+    protected MoveBuilder newMoveBuilder(String pos) {
+        return new MoveBuilder().setOrigin(new Position(pos)).setPiece(this);
+    }
 
-            if (this.player == Player.White) {
-                abbr = abbr.toLowerCase();
-            } else {
-                abbr = abbr.toUpperCase();
-            }
-
-            return abbr;
-        }
-
-        public abstract List<Position> listMoves(String position);
-
-        abstract boolean move(String to);
+    abstract boolean move(String to);
 }
