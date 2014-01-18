fadace4161040c7fba4445bc12dbe9decafa1062
Tony Holdstock-Brown <tonyhb@gmail.com>
2014-01-17 22:34:43 -0500

    Remove any move searching from the Position class.
    No longer necessary with our MoveBuilder. This was the same issue we had before
    the MoveBuilder anyways – tedious looping.

--- src/main/java/chess/Position.java
+++ src/main/java/chess/Position.java
@@ -18,9 +18,8 @@ public class Position {
      * @param position  The current position, as a string
      */
     public Position(String position) {
-        this.row = Integer.parseInt(position.substring(0));
         this.column = position.charAt(0);
-        // Math.abs(Character.compare(GameState.MIN_COLUMN, position.charAt(0)));
+        this.row = Integer.parseInt(position.substring(1));
     }
 
     public Position(char column, int row) {
@@ -35,35 +34,6 @@ public class Position {
      */
     public String toString() {
         return String.valueOf(column) + row;
-    }
-
-    /**
-     * Returns all squares north of the current position until a piece is found or the board is finished.
-     *
-     */
-    public List<Position> searchNorth() {
-        searchNorth(-1);
-    }
-
-    /**
-     * Returns up to N squares north of the current position until a piece is found or the board is finished.
-     * @param limit
-     * @return
-     */
-    public List<Position> searchNorth(int limit, bool include) {
-        List<Position> list = new ArrayList<Position>();
-        if (limit < 0) {
-            int maxDistance = GameState.MAX_ROW;
-        } else {
-            int maxDistance = (this.row + limit) > GameState.MAX_ROW ? GameState.MAX_ROW : this.row + limit;
-        }
-        for (int n = position.row + 1; n <= maxDistance; n++) {
-            Position newPos = new Position(this.column, n);
-            if (GameState.isPieceAt(newPos)) {
-                return list;
-            }
-        }
-        return list;
     }
 
 }
